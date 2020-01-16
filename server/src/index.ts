c33adc7626cd572ea0974ac6e3e5bbf5f759d935
Gabriel Signoretti <gabrielm.sig@gmail.com>
2020-01-16 20:05:17 -0300

    Finished section 14

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import express, {Request, Response} from 'express';
+import {router} from './routes/loginRoutes'
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi There</h1>
-    </div>
-  `);
-});
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['qwewqe'] }));
+// routers
+app.use(router);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
