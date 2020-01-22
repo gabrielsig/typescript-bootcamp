f17e13cd434cefb75758ebca2850654f45e683a1
Gabriel Signoretti <gabrielm.sig@gmail.com>
2020-01-22 12:28:26 -0300

    Basic redux setup

---
+++ react-redux-typescript/src/components/App.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+interface AppProps {
+  color?: string;
+}
+
+export class App extends Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
+
--- react-redux-typescript/src/index.tsx
+++ react-redux-typescript/src/index.tsx
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+import { reducers } from './reducers';
+import { App } from './components/App';
 
-interface AppProps {
-  color?: string;
-}
-
-
-class App extends Component<AppProps> {
-  state = { counter: 0 };
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk),
+);
 
 ReactDOM.render(
-  <App color="red"/>,
+  <Provider store={store}>
+    <App color="red"/>
+  </Provider>,
   document.querySelector('#root'),
 );
 
